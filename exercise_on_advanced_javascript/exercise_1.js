747a473c4cb381437d27bfd3e1befa6e2193617e
swapnanilWebDeveloper <swapnanildev123@gmail.com>
2024-03-01 23:13:45 +0530

    advanced Javascript practiced...!!!

---
+++ exercise_on_advanced_javascript/exercise_1.js
@@ -0,0 +1,11 @@
+
+          console.log("I am a "+job);
+          var job = "web developer";
+          console.log("I am a "+job);
+
+          function WhoAmI(){
+            console.log("I am a "+job);
+            var job = "Full Stack";
+            console.log("I am a "+job);
+          }
+          WhoAmI();
